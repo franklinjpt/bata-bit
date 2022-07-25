aec84921e4f54b2a7f1744cdea6f28935f07c8b5
Franklin <franklinpinedat@gmail.com>
2022-07-25 15:47:45 -0500

    plans scrollbar setted

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 const blackRigthArrow = document.querySelector('#black-rigth-arrow');
 const blackLeftArrow = document.querySelector('#black-left-arrow');
 
+const plansContainer = document.querySelector('.plans-container__slider')
 const tableContainer = document.querySelector('.main-exchange-container__table');
-const tables = document.querySelectorAll('.table-container');
 const table1 = document.querySelector('#table1').clientWidth;
-const size = tables[0].clientWidth;
-console.log(size);
+const plan = document.querySelector('.plans-container__card').clientWidth;
+const size = table1;
+const positionPlan = plan;
 
-// tableContainer.scrollLeft = size;
+plansContainer.scrollLeft = positionPlan;
 
 blackRigthArrow.addEventListener('click', function() {
   tableContainer.scrollLeft += size+table1;
