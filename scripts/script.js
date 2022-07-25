f9aa14a495755a773362bb216bf3e5d487813371
Franklin <franklinpinedat@gmail.com>
2022-07-25 15:55:03 -0500

    javascript fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
-const blackRigthArrow = document.querySelector('#black-rigth-arrow');
-const blackLeftArrow = document.querySelector('#black-left-arrow');
+document.addEventListener('DOMContentLoaded', () => {
+  const blackRigthArrow = document.querySelector('#black-rigth-arrow');
+  const blackLeftArrow = document.querySelector('#black-left-arrow');
 
-const plansContainer = document.querySelector('.plans-container__slider')
-const tableContainer = document.querySelector('.main-exchange-container__table');
-const table1 = document.querySelector('#table1').clientWidth;
-const plan = document.querySelector('.plans-container__card').clientWidth;
-const size = table1;
-const positionPlan = plan;
+  const plansContainer = document.querySelector('.plans-container__slider')
+  const tableContainer = document.querySelector('.main-exchange-container__table');
+  const table1 = document.querySelector('#table1').clientWidth;
+  const plan = document.querySelector('.plans-container__card').clientWidth;
+  const size = table1;
+  const positionPlan = plan;
 
-plansContainer.scrollLeft = positionPlan;
+  plansContainer.scrollLeft = positionPlan;
 
-blackRigthArrow.addEventListener('click', function() {
-  tableContainer.scrollLeft += size+table1;
+  blackRigthArrow.addEventListener('click', function() {
+    tableContainer.scrollLeft += size+table1;
+  })
+  blackLeftArrow.addEventListener('click', function() {
+    tableContainer.scrollLeft -= size+table1;
 })
-blackLeftArrow.addEventListener('click', function() {
-  tableContainer.scrollLeft -= size+table1;
 })
